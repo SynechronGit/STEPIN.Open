3103d789be2e0e768fd2f81d080ea4c17f82bcdf
syn-maheshchitroda <mahesh.chitroda@synechron.com>
2016-09-30 17:12:42 +0530

    IQT-55 As a user I should be able to create, edit and display workspace card
    Added property _id in workspace.cs file

--- STEPIN.Open/STEPIN.Entities/Workspace.cs
+++ STEPIN.Open/STEPIN.Entities/Workspace.cs
@@ -9,6 +9,7 @@ namespace STEPIN.Entities
     public class Workspace
     {
 
+        public Guid _id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
