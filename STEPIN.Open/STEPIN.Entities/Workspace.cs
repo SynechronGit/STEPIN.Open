ba9017b187e27aec7571030a32167445fee858b1
syn-maheshchitroda <mahesh.chitroda@synechron.com>
2016-10-03 14:29:28 +0530

    IQT-55 As a user I should be able to create, edit and display workspace card Remove CreatedDate field

--- STEPIN.Open/STEPIN.Entities/Workspace.cs
+++ STEPIN.Open/STEPIN.Entities/Workspace.cs
@@ -12,7 +12,6 @@ namespace STEPIN.Entities
         public Guid _id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedDate { get; set; }
         public List<Project> Projects { get; set; }
 
         public List<Workflow> Workflows { get; set; }
