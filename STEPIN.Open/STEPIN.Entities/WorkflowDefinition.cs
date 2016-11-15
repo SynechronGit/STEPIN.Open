0950b7565b0612c5786b46fccdf523709f264456
YograjBere <yograj.bere@gmail.com>
2016-11-15 19:09:07 +0530

    Added workflowDefinition entity

---
+++ STEPIN.Open/STEPIN.Entities/WorkflowDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    /// <summary>
+    /// Describes the diagram reprentation of the workflow entity
+    /// </summary>
+    public class WorkflowDefinition
+    {
+        public int _id { get; set; }
+        public int WorkflowId { get; set; }
+        public string Definition { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string Version { get; set; }
+    }
+}
