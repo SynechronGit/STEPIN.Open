2b5d2e6963dfdd1b4dd562bb62b21488c0a2d0e5
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2016-11-15 20:19:46 +0530

    IQT-105 :- Trigger classes added to Stepin.Open Entities

---
+++ STEPIN.Open/STEPIN.Entities/EmailTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace STEPIN.Entities
+{
+    public class EmailTrigger
+    {
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// Imap/Pop Incoming Server Name 
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Email Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Email Password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Server Port
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Is SSL to be used
+        /// </summary>
+        public bool IsSSL { get; set; }
+
+        /// <summary>
+        /// Secure
+        /// </summary>
+        public bool Secure { get; set; }        
+
+        /// <summary>
+        /// Skip SSL Validation
+        /// </summary>
+        public bool SkipSSLValidation { get; set; }
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/FileTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace STEPIN.Entities
+{
+    public class FileTrigger
+    {
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// Trigger File Path
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Trigger file filter
+        /// </summary>
+        public string Filter { get; set; }
+    }
+}
