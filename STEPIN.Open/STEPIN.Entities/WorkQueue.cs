eed2a118dfe53883a4ecb3f05dda6c0815c4ae89
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2016-12-19 18:34:08 +0530

    code added for WorkQueue

---
+++ STEPIN.Open/STEPIN.Entities/WorkQueue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    class WorkQueue
+    {
+        public Guid _id { get; set; }
+        public string WorkQueueName { get; set; }
+        public string Description { get; set; }
+        public int DisplayOrder { get; set; }
+        public string ProcessOrder { get; set; }
+        public string ProcessCycle { get; set; }
+        public string SLA { get; set; }
+
+    }
+}
