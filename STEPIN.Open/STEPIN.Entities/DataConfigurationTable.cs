b1ca92ea818e946d56c5fa756924497f70c21d89
syn-sachinkhamkar <sachin.khamkar@synechron.com>
2017-01-06 18:47:27 +0530

    Added DataConfigurationTable Entity and updated DataSourceConfiguration

---
+++ STEPIN.Open/STEPIN.Entities/DataConfigurationTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+  public class DataConfigurationTable
+    {
+        public Guid _id { get; set; }     
+
+        public string TableName { get; set; }
+
+        public string DataSource { get; set; }
+
+        public string Relationship { get; set; }
+
+        public string PrimaryKey { get; set; }      
+
+        public string DataSourceID { get; set; }
+
+        public string EntityID { get; set; }
+    }
+}
