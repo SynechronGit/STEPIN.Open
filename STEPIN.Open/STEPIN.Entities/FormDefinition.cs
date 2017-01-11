92c59c9e2807723468f7603fa43dd80967ebc831
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-01-11 13:20:06 +0530

    files added for formDefinition and formFieldDefinition

---
+++ STEPIN.Open/STEPIN.Entities/FormDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class FormDefinition
+    {
+        public Guid _id { get; set; }
+
+        public Guid FormId { get; set; }
+
+        public string JSONSchema { get; set; }
+
+        public string ETag { get; set; }
+
+        public int Version { get; set; }
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/FormFieldDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class FormFieldDefinition
+    {
+        public FormFieldDefinition()
+        {
+            items = new List<FormFieldDefinition>();
+        }
+
+        public string key { get; set; }
+        public string type { get; set; }
+        public string style { get; set; }
+        public string title { get; set; }
+        public string onClick { get; set; }
+        public string placeholder { get; set; }
+
+        public string description { get; set; }
+
+        public string htmlClass { get; set; }
+        public List<FormFieldDefinition> items { get; set; }
+    }
+}
