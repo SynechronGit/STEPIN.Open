76b49cb4a732eeb75dd650a62b9dd5864eae5fdc
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-01-16 17:47:39 +0530

    added classes for DomainReference & DomainReferenceItem

---
+++ STEPIN.Open/STEPIN.Entities/DomainReferenceType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class DomainReferenceType
+    {
+        public Guid _id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Schema { get; set; }
+
+        public string SchemaVersion { get; set; }
+
+        public string ItemValueDataType { get; set; }
+
+        public Guid GlobalUID { get; set; }
+
+        public bool ReadOnly { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public List<DomainReferenceItemType> DomainReferenceItemType { get; set; }
+    }
+}
