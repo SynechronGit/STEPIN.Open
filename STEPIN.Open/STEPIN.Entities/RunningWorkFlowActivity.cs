bfe3ab42ec68365f52fa5cc76de1475f6a313afb
syn-sunnysingh <sunnysingh97@gmail.com>
2017-02-01 17:18:02 +0530

    Added Entity for RunningWorkflowActivity

---
+++ STEPIN.Open/STEPIN.Entities/RunningWorkFlowActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class RunningWorkFlowActivity
+    {
+        public Guid _id { get; set; }
+
+        public Guid WorkFlowID { get; set; }
+
+        public string Activity { get; set; }
+
+        public WorkflowState Status { get; set; }
+
+        public string Output { get; set; }
+
+        public Guid AgentId { get; set; }
+    }
+}
