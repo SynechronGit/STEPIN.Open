e8ba25bd9313b01528e716ba22b4876c49da06c9
syn-sachinkhamkar <sachin.khamkar@synechron.com>
2017-02-02 13:23:01 +0530

    Added entity for RunningAgentLog

---
+++ STEPIN.Open/STEPIN.Entities/RunningAgentLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class RunningAgentLog
+    {
+        public Guid _id { get; set; }
+        public DateTime Date { get; set; }
+        public string Severity { get; set; }
+        public string Message { get; set; }
+    }
+}
