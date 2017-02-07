cadb51ecf6adec33edfa0e3afe721f3cc932e5a1
syn-amitkarankar <amit.karankar@synechron.com>
2017-02-07 20:02:44 +0530

    Added entities required for RPA engine.

---
+++ STEPIN.Open/STEPIN.Entities/EmailTriggerMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class EmailTriggerMetaData: TriggerMetaData
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Body { get; set; }
+        public string Message { get; set; }
+        public string Subject { get; set; }
+        
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/TriggerMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public abstract class TriggerMetaData
+    {
+        public int Id { get; set; }
+        public Guid AppletId { get; set; }
+        public Guid EnvironmentId { get; set; }
+        public Guid AgentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+       
+    }
+}
