1ac2eb39fa2d3ad663c04326d4c1e5a08e14dcbe
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-02-17 19:45:26 +0530

    Modified Lock Registry Class

--- STEPIN.Open/STEPIN.Entities/LockRegistry.cs
+++ STEPIN.Open/STEPIN.Entities/LockRegistry.cs
@@ -10,6 +10,10 @@ namespace STEPIN.Entities
     {
         public Guid _id { get; set; }
 
+        public Guid EntityId { get; set; }
+
+        public string Key { get; set; }
+
         public string WhoLocked { get; set; }
 
         public DateTime When { get; set; }
