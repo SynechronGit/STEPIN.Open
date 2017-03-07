53c6ad966dd6125ffe3d8e98b716d2cb87434c78
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-03-07 17:47:46 +0530

    #10 - Added AgentConfiguration Class

---
+++ STEPIN.Open/STEPIN.Entities/AgentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class AgentConfiguration
+    {
+        public Guid _id { get; set; }
+
+        public Guid ServiceAgentId {get;set;}
+
+        public string AgentName { get; set; }
+
+        public string MacAddress { get; set; }
+
+        public bool IsProvisioned { get; set; }
+
+    }
+}
