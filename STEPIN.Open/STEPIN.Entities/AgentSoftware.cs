2f79aba8360327554ccb8b4809921e4f43ffcec0
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-03-07 19:04:41 +0530

    #19 - Added AgentSoftware Class to Stepin.Entities

---
+++ STEPIN.Open/STEPIN.Entities/AgentSoftware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class AgentSoftware
+    {
+        /// <summary>
+        /// mongo db document _id
+        /// </summary>
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// Software Version
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Software Url
+        /// </summary>
+        public string BlobUrl { get; set; }
+
+        /// <summary>
+        /// Draft=0/Published=1 status
+        /// </summary>
+        public Status Status { get; set; }
+    }
+}
