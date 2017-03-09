acd37c4eb480d93195ee2a27c3373cf7559ccd23
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-03-09 16:01:47 +0530

    Created EntitySchema & FormSchema

---
+++ STEPIN.Open/STEPIN.Entities/EntitySchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class EntitySchema
+    {
+        public Guid _id { get; set; }
+
+        public Guid EntityId { get; set; }
+
+        public string JSONSchema { get; set; }
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/FormSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class FormSchema
+    {
+        public Guid _id { get; set; }
+
+        public Guid FormId { get; set; }
+
+        public string JSONSchema { get; set; }
+    }
+}
