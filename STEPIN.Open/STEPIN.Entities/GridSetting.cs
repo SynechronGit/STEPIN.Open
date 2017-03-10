4d260bb638a879d87f4c24e4b3fdae2aec913422
syn-amitkarankar <amit.karankar@synechron.com>
2017-03-10 17:14:39 +0530

    Added GridSetting and Excluded FormSchema/EntitySchema

---
+++ STEPIN.Open/STEPIN.Entities/GridSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class GridSetting
+    {
+        public Guid _id { get; set; }
+
+        public string CollectionName { get; set; }
+
+        public string JSONSchema { get; set; }
+    }
+}
