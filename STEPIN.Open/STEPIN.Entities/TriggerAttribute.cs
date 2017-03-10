2410f515688af196b34967fc6e948a001fb0faf1
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-03-10 16:58:02 +0530

    #6 - Added classes for TriggerAttribute & ValidateAttributeMapping

---
+++ STEPIN.Open/STEPIN.Entities/TriggerAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace STEPIN.Entities
+{
+    public class TriggerAttribute
+    {
+        public Guid _id { get; set; }
+
+        public string AttributeName { get; set; }
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/VariableAttributeMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class VariableAttributeMapping
+    {
+        public Guid _id { get; set; }
+
+        public Guid IFTTTAppletId { get; set; }      
+
+        public Guid VariableId { get; set; }
+
+        public Guid TriggerAttributeId { get; set; }
+    }
+}
