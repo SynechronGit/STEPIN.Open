f737436e6e9bb01a090c780c81a3268f8e61cf0a
syn-sachinkhamkar <sachin.khamkar@synechron.com>
2017-03-15 20:23:24 +0530

    Added System.ComponentModel.DataAnnotations
    Implemented [Key] attribute on ActionSoftware _id

--- STEPIN.Open/STEPIN.Entities/AgentSoftware.cs
+++ STEPIN.Open/STEPIN.Entities/AgentSoftware.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace STEPIN.Entities
         /// <summary>
         /// mongo db document _id
         /// </summary>
+        
+            [Key]
         public Guid _id { get; set; }
 
         /// <summary>
