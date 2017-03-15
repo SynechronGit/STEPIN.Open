b9f86b006dfffd0c4c95900437a92d566d388fd0
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-03-15 12:31:35 +0530

    added FormSchema

--- STEPIN.Open/STEPIN.Entities/FormSchema.cs
+++ STEPIN.Open/STEPIN.Entities/FormSchema.cs
@@ -8,10 +8,17 @@ namespace STEPIN.Entities
 {
     public class FormSchema
     {
+        public FormSchema()
+        {
+            FormFields = new List<FormFieldDefinition>();
+        }
         public Guid _id { get; set; }
 
-        public Guid FormId { get; set; }
+        //public Guid FormFieldDefinitionId { get; set; }
 
-        public string JSONSchema { get; set; }
+        public Guid FormId { get; set; }
+        public List<FormFieldDefinition> FormFields { get; set; }
+        
+        
     }
 }
