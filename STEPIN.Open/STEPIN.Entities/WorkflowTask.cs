eaa32cbfae7a674f3e3cfce99bfbeb6467c34f28
syn-pradeepdhanrajani <pradeepd@synechron.com>
2017-03-19 22:30:35 -0400

    added workflow task class

---
+++ STEPIN.Open/STEPIN.Entities/WorkflowTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class WorkflowTask
+    {
+        public Guid _id { get; set; }
+
+        public Guid WorkflowId { get; set; }
+
+        public Guid WorkflowInstanceId { get; set; }
+
+        public Guid WorkflowTaskId { get; set; }
+
+    }
+}
