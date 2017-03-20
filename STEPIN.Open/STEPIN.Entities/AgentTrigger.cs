641074411d812dbc1c0d1e7b0f9b621fd5c89193
syn-pradeepdhanrajani <pradeepd@synechron.com>
2017-03-20 19:19:52 -0400

    Added new entity - Agent Trigger

---
+++ STEPIN.Open/STEPIN.Entities/AgentTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class AgentTrigger
+    {
+        public List<EmailTrigger> EmailTrigger { get; set; }
+        public List<FileTrigger> FileTrigger { get; set; }
+        public List<TimeTrigger> TimeTrigger { get; set; }
+    }
+}
