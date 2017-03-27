89de47b60d197782e98b765bb1dc8d3893b54d28
syn-pradeepdhanrajani <pradeepd@synechron.com>
2017-03-27 17:04:16 -0400

    Added new entity for action script to be executed

---
+++ STEPIN.Open/STEPIN.Entities/PublishedActionScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class PublishedActionScript
+    {
+        public Guid ActionScriptId { get; set; }
+
+        public string ScriptName { get; set; }
+
+        public string ScriptDescription { get; set; }
+
+        public int ScriptEngine { get; set; } //TC or Python
+
+        public string VersionLabel { get; set; }
+        public string Description { get; set; }
+        public string AzureBlobUrl { get; set; }
+        public DateTime? StartOn { get; set; }
+        public DateTime? EndOn { get; set; }
+
+        public string PythonScriptContent { get; set; }
+
+        public string ProjectFileName { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectItemName { get; set; }
+        
+    }
+}
