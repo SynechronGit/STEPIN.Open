6e64221c88d2979ba4aef07f3edf66603eab9e25
syn-pradeepdhanrajani <pradeepd@synechron.com>
2017-03-27 23:33:12 -0400

    new entities for blob storage

---
+++ STEPIN.Open/STEPIN.Entities/CommonBlobMaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class CommonBlobMaster
+    {
+        public Guid _id { get; set; }
+
+        public string BlobPath { get; set; }
+    }
+}
