ba84794353721a80e0708170f20dfb698dd6f30d
syn-pradeepdhanrajani <pradeepd@synechron.com>
2017-03-28 23:55:00 -0400

    added target application to the entity

--- STEPIN.Open/STEPIN.Entities/PublishedActionScript.cs
+++ STEPIN.Open/STEPIN.Entities/PublishedActionScript.cs
@@ -9,24 +9,18 @@ namespace STEPIN.Entities
     public class PublishedActionScript
     {
         public Guid ActionScriptId { get; set; }
-
         public string ScriptName { get; set; }
-
         public string ScriptDescription { get; set; }
-
         public int ScriptEngine { get; set; } //TC or Python
-
         public string VersionLabel { get; set; }
         public string Description { get; set; }
         public string AzureBlobUrl { get; set; }
         public DateTime? StartOn { get; set; }
         public DateTime? EndOn { get; set; }
-
         public string PythonScriptContent { get; set; }
-
         public string ProjectFileName { get; set; }
         public string ProjectName { get; set; }
         public string ProjectItemName { get; set; }
-        
+        public List<TargetApplication> TargetApplications { get; set; }
     }
 }
