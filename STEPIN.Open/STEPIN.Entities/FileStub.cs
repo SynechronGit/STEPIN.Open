93623c80324fc00750755a9dfff1f121a44a6511
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-04-03 22:01:08 +0530

    added comment for mongo db _id

--- STEPIN.Open/STEPIN.Entities/FileStub.cs
+++ STEPIN.Open/STEPIN.Entities/FileStub.cs
@@ -4,6 +4,9 @@ namespace STEPIN.Entities
 {
     public class FileStub
     {
+        /// <summary>
+        /// mongo db requires Id in format _id 
+        /// </summary>
         public Guid _id { get; set; }
 
         public string Folder { get; set; }
