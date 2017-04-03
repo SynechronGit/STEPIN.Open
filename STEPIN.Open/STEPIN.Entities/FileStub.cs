57b1525364ec5b7ed8d721494528926a18eed3a4
syn-rushikeshbiradar <rushikesh.biradar@synechron.com>
2017-04-03 19:29:35 +0530

    #62 - Added classes required for filesystemstub api

---
+++ STEPIN.Open/STEPIN.Entities/FileStub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace STEPIN.Entities
+{
+    public class FileStub
+    {
+        public Guid Id { get; set; }
+
+        public string Folder { get; set; }
+
+        public string FileName { get; set; }
+
+        public long SizeInBytes { get; set; }
+
+        public string Hash { get; set; }
+    }
+}
---
+++ STEPIN.Open/STEPIN.Entities/FileSystemStub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class FileSystemStub
+    {
+        public FileSystemStub()
+        {
+            Files = new List<FileStub>();
+        }
+
+        public string TriggerID { get; set; }
+
+        public DateTime LastUpdatedOn { get; set; }
+
+        public List<FileStub> Files { get; set; }
+    }
+}
