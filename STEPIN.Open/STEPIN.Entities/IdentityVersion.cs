ec9ee9310366f0dc976a2c0104321c5b70d24d3a
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-04-13 13:16:31 +0530

    Added Entity for Identity and IdentityVersion

---
+++ STEPIN.Open/STEPIN.Entities/IdentityVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class IdentityVersion
+    {
+        Guid _id { get; set; }
+        Guid IdentityId { get; set; }
+        int Version { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Domain { get; set; }
+        public string UserName { get; set; }       
+        public string PasswordScrete { get; set; }
+        public string PasswordHash { get; set; }
+        public string Certificate { get; set; }
+    }
+}
