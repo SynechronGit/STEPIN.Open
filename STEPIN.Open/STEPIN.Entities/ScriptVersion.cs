65055fc877500c6cc480f07e2107e3a3fdc37014
syn-AnujaPhulsunder <anuja.phulsunder@synechron.com>
2017-04-18 19:51:33 +0530

    Added ScriptVersion entity

---
+++ STEPIN.Open/STEPIN.Entities/ScriptVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPIN.Entities
+{
+    public class ScriptVersion
+    {
+        public Guid _id { get; set; }
+        public Guid ActionScriptId { get; set; }
+        public string VersionLabel { get; set; }
+        public int Status { get; set; }
+        public string ScriptContent { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
