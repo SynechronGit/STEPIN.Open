2a38259f82104cba469e6d35368467339fbf0f18
syn-amitkarankar <amit.karankar@synechron.com>
2017-04-19 15:41:10 +0530

    Added entity ActionScriptVersionPackage

---
+++ STEPIN.Open/STEPIN.Entities/ActionScriptVersionPackage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STEPIN.Entities
+{
+    /// <summary>
+    /// This entity stores Mapping Between ActionScript Version and Package
+    /// one ActionScript Version to have one or multiple Package
+    /// </summary>
+    public class ActionScriptVersionPackage
+    {
+        /// <summary>
+        /// mongo db collection _id
+        /// </summary>
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// ActionScript Version Id
+        /// </summary>
+        public Guid ActionScriptVersionId { get; set; }
+
+        /// <summary>
+        /// Package Id
+        /// </summary>
+        public List<Guid> PackageId { get; set; }
+    }
+}
