626a6a7c4494cd5562056bfbe53cb310de3b7a11
syn-amitkarankar <amit.karankar@synechron.com>
2017-04-26 19:21:29 +0530

    Added Entity PackageTree and removed ActionScriptVersionPackage

---
+++ STEPIN.Open/STEPIN.Entities/PackageTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STEPIN.Entities
+{
+    /// <summary>
+    /// Stores the Dependencies of the Package/Action Script
+    /// </summary>
+    public class PackageTree
+    {
+        public Guid PackageId { get; set; }
+        public string PackageName { get; set; }
+        public List<PackageTree> PackageDependencies { get; set; }
+        public PackageVersion LatestPublishedVersion { get; set; }
+    }
+}
